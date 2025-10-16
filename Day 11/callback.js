eb2754be850268cd9f52d92612564c9ce3f6ff79
Jenifer29b <Jenifer29b@gmail.com>
2025-10-16 14:22:13 +0530

    Day11 of callback

--- Day 11/callback.js
+++ Day 11/callback.js
@@ -1,3 +1,8 @@
+// callback is function in Javascript that takes as an argument by other function .. 
+// another function taskes this function as an argument  and exceutes it 
+
+
+// normal calback function with user given names
 function greet(name, callback) {
     console.log("hello!!", name)
     callback()
@@ -5,5 +10,40 @@ function greet(name, callback) {
 function callback() {
     console.log("good bye")
 }
+greet("ruban", callback)
 
-greet("ruban" , callback)
+// callback with user given age by passing the arguments to callback
+function number(age, callback) {
+    console.log("hii I am ruban")
+    callback(age) // age is passed to another function by using callback
+}
+function hello(age) {
+    console.log(`my age is ${age}`) // that passed age i collected by this function
+}
+number(20, hello)
+
+
+// callback function using accessing the inside objects
+function obj(callback) {
+    let variables = {
+        "name": "ruban",
+        "age" : 21
+    }
+    callback(variables)
+}
+let objvar = (variables) => {
+    console.log(`Hiii!! I am ${variables.name}.. I am ${variables.age} years old `)
+}
+obj(objvar)
+
+function arr(callback) {
+    let num = [1, 2, 3, 4, 5]
+    callback(num)
+}
+
+let array = (num) => {
+    for (arrnum of num) {
+        console.log(arrnum*3)
+    }
+}
+arr(array)
