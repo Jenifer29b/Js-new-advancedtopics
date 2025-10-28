fdfb5618cf835c2fd075635d37a9e1a9e31cab17
Jenifer29b <Jenifer29b@gmail.com>
2025-10-28 18:27:58 +0530

    Day15 : array methods

---
+++ Day 15/arraymethod.js
@@ -0,0 +1,17 @@
+// push is method in array is used to add one or more elements in end of an array
+//pop is used to delete an last elemnent in an array
+
+let arr1 = ["apple", "mango", "kiwi"]
+console.log(arr1)
+arr1.push("banana","papaya")
+console.log(arr1)
+arr1.pop()
+console.log(arr1)
+
+//unshift is used to add elements in starting of an array
+// shift is used to remove first element in an array
+let arr2 = ["car", "bus", "bike"]
+arr2.unshift("lorry","truck")
+console.log(arr2)
+arr2.shift()
+console.log(arr2)
