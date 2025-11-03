ab7edeb54ed900fc871a3f4f233af6e19d890f6d
Jenifer29b <Jenifer29b@gmail.com>
2025-11-03 17:19:15 +0530

    basic tasks of day 16

--- Day 16/task1.js
+++ Day 16/task1.js
@@ -4,3 +4,23 @@ let arr1 = [1, 2, 3, 4, 5, 6]
 
 let totalvalues = arr1.map((value) => value * 3).filter((value) => value % 2 == 0)
 console.log(totalvalues)
+
+// sum calculator
+
+let numbers = [11, 22, 33, 55, 66, 88, 99, 100]
+
+let sum = numbers.reduce((prev, curr) => {
+    return (prev+curr)
+})
+console.log(sum)
+
+//word calcuator
+
+let word = "apple mango apple banana mango apple kiwi"
+let words = word.split(" ")
+
+let counts = words.reduce((prev, curr) => {
+    prev[curr] = (prev[curr] || 0) + 1
+    return prev
+}, {})
+console.log(counts)
